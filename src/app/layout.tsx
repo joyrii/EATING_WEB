5804b6bbc286343ddc158a89f97caf93537372b7
yeseul <joyri0202@gmail.com>
2026-01-19 19:52:43 +0900

    chore: setup styled-components

---
+++ src/app/layout.tsx
@@ -0,0 +1,38 @@
+import type { Metadata, Viewport } from "next";
+import "./globals.css";
+import StyledComponentsRegistry from "../lib/styled-components/registry";
+
+export const metadata: Metadata = {
+  title: "EATING - 이화여대 밥약 매칭",
+  description: "이화여대 학생들을 위한 밥약 매칭 서비스",
+};
+
+export const viewport: Viewport = {
+  width: "device-width",
+  initialScale: 1,
+  maximumScale: 1,
+  userScalable: false,
+  viewportFit: "cover",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="ko">
+      <head>
+        <link
+          rel="stylesheet"
+          as="style"
+          crossOrigin="anonymous"
+          href="https://cdn.jsdelivr.net/gh/orioncactus/pretendard@v1.3.9/dist/web/variable/pretendardvariable.min.css"
+        />
+      </head>
+      <body className="antialiased max-w-[430px] mx-auto bg-[#FFFEFE] min-h-screen">
+        <StyledComponentsRegistry>{children}</StyledComponentsRegistry>
+      </body>
+    </html>
+  );
+}
